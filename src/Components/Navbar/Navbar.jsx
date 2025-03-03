3b38d51aba8a05c84f062996642a9a25f133fd7a
ragulnathMB <ragulnath255@gmail.com>
2025-03-03 09:34:04 +0530

    updated files

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -29,7 +29,7 @@ const Navbar=({contactRef,homeRef})=>{
                 <div style={{marginBottom:-30}}><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill={(theme==='Dark')?"white":"black"}><path d="M160-160q-33 0-56.5-23.5T80-240v-480q0-33 23.5-56.5T160-800h640q33 0 56.5 23.5T880-720v480q0 33-23.5 56.5T800-160H160Zm320-280L160-640v400h640v-400L480-440Zm0-80 320-200H160l320 200ZM160-640v-80 480-400Z"/></svg></div>
                 <div style={{marginBottom:-10}}><p>Contact</p></div>
             </div>
-            <a className={styles.item} href="https://drive.google.com/file/d/1_UzPyH7Nl_n-LuijxURmg6YPo-4w9enG/view?usp=sharing" target="to_blank">
+            <a className={styles.item} href="https://drive.google.com/file/d/1AGNKX87GB2v9QTNuVJPYEhZB_k9_KTZb/view?usp=sharing" target="to_blank">
                 <div style={{marginBottom:-30}}><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill={(theme==='Dark')?"white":"black"}><path d="M320-240h320v-80H320v80Zm0-160h320v-80H320v80ZM240-80q-33 0-56.5-23.5T160-160v-640q0-33 23.5-56.5T240-880h320l240 240v480q0 33-23.5 56.5T720-80H240Zm280-520v-200H240v640h480v-440H520ZM240-800v200-200 640-640Z"/></svg></div>
                 <div style={{marginBottom:-10}}><p>Resume</p></div>
             </a>
